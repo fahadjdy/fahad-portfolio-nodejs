9fe9bc5f1fb338f81218c0c9683514b1e1b5450a
fahad.jadiya <fahad.jadiya@techaxion.com>
2025-08-29 10:20:42 +0530

    login link added

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -2,7 +2,8 @@
     <div class="bg-white p-6 rounded-xl shadow transition-all">
       <h2 class="text-lg font-semibold text-gray-800 flex gap-2 items-center">
         <i class="fas fa-trophy text-yellow-500"></i>
-        Welcome to your dashboard!
+        Welcome to your dashboard! 
+        <router-link to="/login"> Login Now </router-link>
       </h2>
       <p class="mt-2 text-gray-600">This is a modern, responsive admin interface using Tailwind CSS and FontAwesome—try it on mobile!</p>
     </div>
